027babd3476c07172ff9c6ce283f808112d7fa9c
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-19 16:49:42 +0200

    ft: add test cases

--- studentMs/src/main/java/com/rw/studentMs/dto/CreateStudentDto.java
+++ studentMs/src/main/java/com/rw/studentMs/dto/CreateStudentDto.java
@@ -2,12 +2,14 @@ package com.rw.studentMs.dto;
 
 import com.rw.studentMs.enums.EStudentDepartment;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CreateStudentDto {
     private String name;
     private String email;
