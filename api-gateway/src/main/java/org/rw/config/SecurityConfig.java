3c507f7836a1e21953b01514441fd277f5d01ef8
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-09-05 15:31:05 +0200

    fix: api gateway security config

--- api-gateway/src/main/java/org/rw/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/rw/config/SecurityConfig.java
@@ -1,25 +1,28 @@
-//package org.rw.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-//        serverHttpSecurity
-//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .authorizeExchange(exchange ->
-//                        exchange.pathMatchers("/eureka/**")
-//                                .permitAll()
-//                                .anyExchange()
-//                                .authenticated())
-//                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
-//        return serverHttpSecurity.build();
-//    }
-//}
+package org.rw.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/eureka/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
