eff721ef3f1b2e5a261e553013dcf6802afaf727
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-09-05 15:06:07 +0200

    ft: add eureka discovery server

--- api-gateway/src/main/java/org/rw/ApiGateWayApplication.java
+++ api-gateway/src/main/java/org/rw/ApiGateWayApplication.java
@@ -3,10 +3,8 @@ package org.rw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class ApiGateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGateWayApplication.class, args);
